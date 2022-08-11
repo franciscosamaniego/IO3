bdd525e89c588fcc42cbc23421bdcd9adbd09d3e
franciscosamaniego <102254119+franciscosamaniego@users.noreply.github.com>
2022-08-11 10:29:06 -0300

    Update ej10.c

--- ej10.c
+++ ej10.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 
 int main (int argc, char *argv[]) {
-    char *palabras[1000];
-    int lineas = 0;
-    int i = 0;
-    while (scanf("%s", &palabras) != '\0') {
-      if(palabras[i] == "\n")     lineas++;
-    }
-    printf("Lineas: %d\n", lineas);
+  char *filename = argv[1];
+  FILE *fp;
+  fp = fopen(filename, "r");
+  int c;
+  int line = 0;
+  while((c = fgetc(fp)) != EOF) {
+    if(c == '\n')   line++;
+  }
+  printf("Lineas: %d\n", line);
     return 0;
 }
+
